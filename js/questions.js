91aa8e7ab9a39b89838c2e198309e5b825af697c
kamontander <matej.korlaet@gmail.com>
2022-03-29 14:16:18 +0200

    Commint on 'main'

---
+++ js/questions.js
@@ -0,0 +1,103 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+      numb: 1,
+      question: "Jednokratna plastična vrećica razgrađuje se koliko dugo?",
+      answer: "i do 500 godina",
+      options: [
+        "9 mjeseci",
+        "100 godina",
+        "i do 500 godina"
+      ]
+    },
+    {
+      numb: 2,
+      question: "Kako zovemo štetne komadiće koji nastaju razgradnjom jednokratne plastike i manji su od 5 mm?",
+      answer: "mikroplastika",
+      options: [
+        "nanoplastika",
+        "mikroplastika",
+        "mini botox"
+      ]
+    },
+    {
+      numb: 3,
+      question: "Prema nekim procjenama u morima će do 2050. biti više plastike nego…",
+      answer: "ribe",
+      options: [
+        "ribe",
+        "školjki",
+        "vode"
+      ]
+    },
+    // {
+    //   numb: 4,
+    //   question: "Ako redovno koristiš jednokratne plastične vrećice koliko ih u prosjeku upotrijebiš tijekom života?",
+    //   answer: "oko 16 000",
+    //   options: [
+    //     "ravno 666",
+    //     "otprilike 10 000",
+    //     "oko 16 000"
+    //   ]
+    // },
+    // {
+    //   numb: 5,
+    //   question: "Iako plastika štetno djeluje na sav živući svijet koje životinje su najugroženije zbog jednokratne plastike i vrećica?",
+    //   answer: "morske životinje i ptice (kornjače, ptice, ribe i morski sisavci)",
+    //   options: [
+    //     "čovječja ribica",
+    //     "morske životinje i ptice (kornjače, ptice, ribe i morski sisavci)",
+    //     "čudnovati kljunaši"
+    //   ]
+    // },
+    // {
+    //   numb: 6,
+    //   question: "U prosjeku samo jedna višekratna torba može zamijeniti bar jednokratnih plastičnih vrećica?",
+    //   answer: "1000",
+    //   options: [
+    //     "tri",
+    //     "500",
+    //     "1000"
+    //   ]
+    // },
+    // {
+    //   numb: 7,
+    //   question: "Kako svijetu ide recikliranje jednokratnih plastičnih vrećica?",
+    //   answer: "sjedi 1, reciklira se manje od 1% jednokratnih plastičnih vrećica",
+    //   options: [
+    //     "sjedi 1, reciklira se manje od 1% jednokratnih plastičnih vrećica",
+    //     "ma za 5, većina jednokratnih plastičnih vrećica se reciklira",
+    //     "može i bolje, reciklira se oko 50%  jednokratnih plastičnih vrećica"
+    //   ]
+    // },
+    // {
+    //   numb: 8,
+    //   question: "Velika pacifička mrlja otpada koji pluta između kalifornije i havaja je površinom veća od hrvatske koliko veća?",
+    //   answer: "veličine je oko 1.6 milijuna m², dakle oko 29 puta veća",
+    //   options: [
+    //     "veličine je oko 1.6 milijuna m², dakle oko 29 puta veća",
+    //     "radim u marketingu, nemojte me s matematikom",
+    //     "veličine je oko pola milijuna m², dakle 10 puta veća"
+    //   ]
+    // },
+    // {
+    //   numb: 9,
+    //   question: "Što svatko od nas sigurno može učiniti za smanjenje količine jednokratne plastike u okolišu?",
+    //   answer: "koristiti višekratne torbe, za početak",
+    //   options: [
+    //     "koristiti višekratne torbe, za početak",
+    //     "lijepo slagati jednokratne plastične vrećice u smeće da izgleda kao da ih je manje",
+    //     "jednokratnu plastiku bacati kod susjeda "
+    //   ]
+    // },
+    // {
+    //   numb: 10,
+    //   question: "Morske kornjače jedna su od najugroženijih skupina životinja na svijetu, a tu spadaju i zaštićene glavate želve. Nažalost, 2020. godine zbog gutanja plastičnih čepova uginula je glavata želva pronađena na području Lastova. Bila je stara oko 25 godina, težila 40 kilograma i mi ne želimo da se ona zaboravi… Njeno ime bilo je",
+    //   answer: "Huanita",
+    //   options: [
+    //     "Jozefina",
+    //     "Huanita",
+    //     "Maruška"
+    //   ]
+    // }
+];
